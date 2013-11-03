5235675c549e99fd0bcff5b59c5915e6c938719a
ryf123 <ryf123@gmail.com>
2013-11-03 15:20:57 -0800

    index php and style css

---
+++ index.php
@@ -0,0 +1,28 @@
+
+<html>
+<head>
+<title>UCSD1SHOP</title>
+<link href="css/style.css" media="all" rel="stylesheet" type="text/css" />
+<link href="css/style2.css" media="screen" rel="stylesheet" type="text/css" />
+</head>
+<style>
+</style>
+<body>
+<div class="wrap">
+	<nav>
+		<ul class="menu">
+			<li><a href="index.php"><span class="icon home"></span> UCSD1SHOP</a></li>
+			<li><a href="login.php"><span class="icon"></span> Login</a></li>
+			<li><a href="create_account.php"><span class="icon"></span> New Acccount</a></li>
+		</ul>
+		<div class="clearfix"></div>
+	</nav>
+</div>
+<div class="gallery_item">
+	<form name="cart_form" method="post" action="index.php">
+	</form>
+</div>
+<div class="labelContainer">
+<div class="labelArrow"></div>
+</body>
+</html>
